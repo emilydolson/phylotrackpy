6bc9e1553e226a432b7b9db7fe8246a12e27a56c
Emily Dolson <EmilyLDolson@gmail.com>
2022-02-18 01:43:35 -0500

    Add tests

---
+++ tests/tests.py
@@ -0,0 +1,23 @@
+import systematics
+
+
+class TestOrg:
+    def __init__(self, genotype):
+        self.genotype = genotype
+
+
+def taxon_info_fun(org):
+    return org.genotype
+
+
+def test_world_position():
+    wp = systematics.WorldPosition(1, 0)
+    assert(wp.getIndex() == 1)
+    assert(wp.getPopID() == 0)
+
+
+def test_systematics():
+    sys = systematics.Systematics(taxon_info_fun)
+    wp = systematics.WorldPosition(1, 0)
+    org = TestOrg("hello")
+    sys.addOrg(org, wp)
