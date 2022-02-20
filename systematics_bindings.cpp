b4f2af5cb96606d3f79f464d9033ec73499d3c43
Emily Dolson <EmilyLDolson@gmail.com>
2022-02-20 17:44:04 -0500

    Add more interface

--- systematics_bindings.cpp
+++ systematics_bindings.cpp
@@ -39,17 +39,51 @@ PYBIND11_MODULE(systematics, m) {
     py::class_<taxon_t, std::unique_ptr<taxon_t, py::nodelete> >(m, "Taxon")
         .def(py::init<size_t, taxon_info_t>())
         .def(py::init<size_t, taxon_info_t, taxon_t*>())
-        .def("getParent", &taxon_t::GetParent);
+        .def("get_parent", &taxon_t::GetParent);
 
     py::class_<sys_t>(m, "Systematics")
         .def(py::init<std::function<taxon_info_t(org_t &)>, bool, bool, bool, bool>(), py::arg("calc_taxon"), py::arg("store_active") = true, py::arg("store_ancestors") = true, py::arg("store_all") = false, py::arg("store_pos") = true)
         // .def("test", [](const sys_t & s, int a, std::optional<int *> b){return a + *b;}, "A function which adds two numbers", py::arg("a"), py::arg("b") = py::none())
-        .def("addOrgByPosition", static_cast<void (sys_t::*) (org_t &, emp::WorldPosition)>(&sys_t::AddOrg), "Add an organism to systematics manager")
-        .def("addOrgByPosition", static_cast<void (sys_t::*) (org_t &, emp::WorldPosition, emp::WorldPosition)>(&sys_t::AddOrg), "Add an organism to systematics manager")
-        // .def("addOrg", static_cast<taxon_ptr (sys_t::*) (org_t &, emp::WorldPosition, taxon_t *)>(&sys_t::AddOrg), "Add an organism to systematics manager", py::return_value_policy::reference_internal)
-        // .def("addOrg", static_cast<taxon_ptr (sys_t::*) (py::handle &, taxon_ptr)>(&sys_t::AddOrg), "Add an organism to systematics manager", py::return_value_policy::reference_internal)
-        .def("addOrg", [](sys_t & self, org_t & org){return self.AddOrg(org, nullptr);}, "Add an organism to systematics manager", py::return_value_policy::reference_internal)
-        .def("addOrg", [](sys_t & self, org_t & org, taxon_t * parent){return self.AddOrg(org, parent);}, "Add an organism to systematics manager", py::return_value_policy::reference_internal)
+
+        // Setting systematics manager state
+        .def("set_calc_info_fun", static_cast<void (sys_t::*) (std::function<taxon_info_t(org_t &)>)>(&sys_t::SetCalcInfoFun))        
+        .def("set_store_active", static_cast<void (sys_t::*) (bool)>(&sys_t::SetStoreActive))        
+        .def("set_store_ancestors", static_cast<void (sys_t::*) (bool)>(&sys_t::SetStoreAncestors))
+        .def("set_store_outside", static_cast<void (sys_t::*) (bool)>(&sys_t::SetStoreOutside))
+        .def("set_store_archive", static_cast<void (sys_t::*) (bool)>(&sys_t::SetArchive))
+        .def("set_store_position", static_cast<void (sys_t::*) (bool)>(&sys_t::SetStorePosition))
+        .def("set_track_synchronous", static_cast<void (sys_t::*) (bool)>(&sys_t::SetTrackSynchronous))
+        .def("set_update", static_cast<void (sys_t::*) (size_t)>(&sys_t::SetUpdate))
+
+        // Getting systematics manager state
+        .def("get_store_active", static_cast<bool (sys_t::*) () const>(&sys_t::GetStoreActive))        
+        .def("get_store_ancestors", static_cast<bool (sys_t::*) () const>(&sys_t::GetStoreAncestors))
+        .def("get_store_outside", static_cast<bool (sys_t::*) () const>(&sys_t::GetStoreOutside))
+        .def("get_store_archive", static_cast<bool (sys_t::*) () const>(&sys_t::GetArchive))
+        .def("get_store_position", static_cast<bool (sys_t::*) () const>(&sys_t::GetStorePosition))
+        .def("get_track_synchronous", static_cast<bool (sys_t::*) () const>(&sys_t::GetTrackSynchronous))
+        .def("get_update", static_cast<size_t (sys_t::*) () const>(&sys_t::GetUpdate))
+        .def("get_total_orgs", static_cast<size_t (sys_t::*) () const>(&sys_t::GetTotalOrgs))
+        .def("get_num_roots", static_cast<size_t (sys_t::*) () const>(&sys_t::GetNumRoots))
+        .def("get_next_id", static_cast<size_t (sys_t::*) () const>(&sys_t::GetNextID))
+        .def("get_ave_depth", static_cast<double (sys_t::*) () const>(&sys_t::GetAveDepth))
+
+        // Birth notification
+        .def("add_org_by_position", static_cast<void (sys_t::*) (org_t &, emp::WorldPosition)>(&sys_t::AddOrg), "Add an organism to systematics manager")
+        .def("add_org_by_position", static_cast<void (sys_t::*) (org_t &, emp::WorldPosition, emp::WorldPosition)>(&sys_t::AddOrg), "Add an organism to systematics manager")
+        .def("add_org", [](sys_t & self, org_t & org){return self.AddOrg(org, nullptr);}, "Add an organism to systematics manager", py::return_value_policy::reference_internal)
+        .def("add_org", [](sys_t & self, org_t & org, taxon_t * parent){return self.AddOrg(org, parent);}, "Add an organism to systematics manager", py::return_value_policy::reference_internal)
+
+        // Death notification
+        .def("remove_org_by_position", static_cast<bool (sys_t::*) (emp::WorldPosition)>(&sys_t::RemoveOrg))
+        .def("remove_org", [](sys_t & self, taxon_t * tax){return self.RemoveOrg(tax);})
+        .def("remove_org_by_position_after_repro", static_cast<void (sys_t::*) (emp::WorldPosition)>(&sys_t::RemoveOrgAfterRepro))
+        .def("remove_org_after_repro", [](sys_t & self, taxon_t * tax){self.RemoveOrgAfterRepro(tax);})
+        .def("set_next_parent_by_position", static_cast<void (sys_t::*) (emp::WorldPosition)>(&sys_t::SetNextParent))
+        .def("set_next_parent", [](sys_t & self, taxon_t * tax){self.SetNextParent(tax);})
+
+
+
         ;
 }
 
--- test_systematics.py
+++ test_systematics.py
@@ -21,19 +21,32 @@ def test_world_position():
 
 def test_systematics_by_position():
     sys = systematics.Systematics(taxon_info_fun, True, True, False, True)
-    wp = systematics.WorldPosition(1, 0)
+    org_pos = systematics.WorldPosition(1, 0)
     org = ExampleOrg("hello")
-    sys.addOrgByPosition(org, wp)
+    sys.add_org_by_position(org, org_pos)
+    child_pos = systematics.WorldPosition(2, 0)
+    child_org = ExampleOrg("hello2")
+    sys.add_org_by_position(child_org, child_pos, org_pos)
+    # sys.remove_org()
 
 
 def test_systematics():
     # sys = systematics.Systematics(taxon_info_fun, True, True, False, False)
-    sys = systematics.Systematics(str, True, True, False, False)
+    sys = systematics.Systematics(taxon_info_fun, True, True, False, False)
     org = ExampleOrg("hello")
     org2 = ExampleOrg("hello 2")
     org_tax = systematics.Taxon(0, "hello")
-    org2_tax = sys.addOrg(org2, org_tax)
+    org2_tax = sys.add_org(org2, org_tax)
+    org3_tax = sys.add_org(org2)
+    org4_tax = sys.add_org(org, org2_tax)
+    org5_tax = sys.add_org(org, org4_tax)
 
+    assert(org2_tax.get_parent() == org_tax)
+    assert(org3_tax.get_parent() is None)
+    assert(org4_tax.get_parent() == org2_tax)
+    assert(org_tax != org2_tax)
+    assert(org4_tax != org2_tax)
+    assert(org5_tax == org4_tax)
 
 if __name__ == "__main__":
     test_systematics()
