448c553373b57ebd11f5aae05d2b670956fbc832
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-11-22 17:20:27 -0500

    Draft profiling experiment

---
+++ profile/profile_batch.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+BATCH_UUID="$(uuid)"
+
+{
+
+###############################################################################
+echo "SETUP"
+echo "########################################################################"
+cd "$(dirname "$0")"
+echo "pwd $(pwd)"
+
+RUN_SECONDS=4
+echo "RUN_SECONDS ${RUN_SECONDS}"
+
+echo "BATCH_UUID ${BATCH_UUID}"
+mkdir -p "batch=${BATCH_UUID}+what=rawdata"
+
+VENV="$(mktemp -d)"
+python3 -m venv "${VENV}"
+source "${VENV}/bin/activate"
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+
+###############################################################################
+echo "MAKE PROVLOG"
+echo "########################################################################"
+  cat << EOF >> "batch=${BATCH_UUID}+what=provlog+ext=.yaml"
+-
+  a: "batch=${BATCH_UUID}+what=provlog+ext=.yaml"
+  batch: ${BATCH_UUID}
+  date: \$(date --iso-8601=seconds)
+  python: $(python3 --version)
+  revision: $(git rev-parse HEAD)
+  hostname: $(hostname)
+  pwd: $(pwd)
+  neofetch:
+$(neofetch --stdout | sed 's/^/    /')
+  os_info: |
+$(lsb_release -a 2>/dev/null || cat /etc/*release | sed 's/^/    /')
+  cpu_info: |
+$(lscpu | sed 's/^/    /')
+  memory_info: |
+$(free -h | sed 's/^/    /')
+  disk_info: |
+$(df -h | sed 's/^/    /')
+  network_info: |
+$(ifconfig -a | sed 's/^/    /')
+  user: $(whoami)
+  uuid: $(uuidgen)
+  slurm_job_id: ${SLURM_JOB_ID-none}
+  profile_batch.sh: |
+$(sed 's/^/    /' profile_batch.sh)
+  profile_job.py: |
+$(sed 's/^/    /' profile_job.py)
+  requirements.txt: |
+$(sed 's/^/    /' requirements.txt)
+EOF
+
+###############################################################################
+echo "RUN JOBS"
+echo "########################################################################"
+for replicate in {0..4}; do for pop_size in 10 1000 100000; do
+    JOB_UUID="$(uuid)"
+    echo "JOB_UUID ${JOB_UUID} replicate ${replicate} pop_size ${pop_size}"
+    mprof run -o "batch=${BATCH_UUID}+what=rawdata/a=memprof+job=${JOB_UUID}+ext=.dat" \
+        python3 profile_job.py \
+        "${BATCH_UUID}" "${JOB_UUID}" \
+        "${replicate}" "${pop_size}"
+done; done
+
+
+###############################################################################
+echo "COLLLATE DATA"
+echo "########################################################################"
+python3 << EOF
+import glob
+
+from keyname import keyname as kn
+import pandas as pd
+
+
+# COLLATE TIME DATA
+###############################################################################
+time_file_list = glob.glob("batch=${BATCH_UUID}+what=rawdata/a=timeprof+job=*+ext=.csv")
+time_concat = pd.concat([pd.read_csv(f) for f in time_file_list])
+time_concat.to_csv("batch=${BATCH_UUID}+what=timeprof+ext=.csv", index=False)
+
+# COLLATE MEMORY DATA
+##############################################################################
+mem_file_list = glob.glob("batch=${BATCH_UUID}+what=rawdata/a=memprof+job=*+ext=.dat")
+
+# Read and concatenate the CSV files
+mem_data = []
+for f in mem_file_list:
+    job = kn.unpack(f)["job"]
+    mem_df = pd.read_csv(
+        f,
+        index_col=False,
+        names=["MEM", "memory (kb)", "timestamp"],
+        sep=" ",
+        skiprows=1,
+    )
+    mem_df["job"] = job
+    mem_df["seconds"] = mem_df["timestamp"] - mem_df["timestamp"].min()
+
+    mem_data.append(mem_df)
+
+mem_concat = pd.concat(mem_data)
+mem_merged = mem_concat.merge(time_concat, how="left", on="job")
+
+# Write the combined data to a new CSV, without index
+mem_merged.to_csv("batch=${BATCH_UUID}+what=memprof+ext=.csv", index=False)
+EOF
+
+
+###############################################################################
+echo "PLOT MEMORY DATA"
+echo "########################################################################"
+python3 << EOF
+import glob
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+from teeplot import teeplot as tp
+
+
+def plot_memprof(*args, **kwargs) -> None:
+    plt.gcf().set_size_inches(5, 3)
+    ax = sns.lineplot(*args, **kwargs)
+    sns.move_legend(ax, "center left", bbox_to_anchor=(1, 1), frameon=False)
+    ax.text(
+        0.05, 0.95,
+        f"n = {kwargs['data']['replicate'].nunique()}",
+        horizontalalignment="left", verticalalignment="top",
+        transform=ax.transAxes,
+    )
+
+
+data = pd.read_csv(
+    "batch=${BATCH_UUID}+what=memprof+ext=.csv",
+    index_col=False,
+)
+data["memory (mb)"] = data["memory (kb)"] / 1000
+data["memory (gb)"] = data["memory (mb)"] / 1000
+for memory in "memory (kb)", "memory (mb)", "memory (gb)":
+    plt.gca().clear()
+    plt.clf()
+    tp.tee(
+        plot_memprof,
+        data=data,
+        x="seconds",
+        y=memory,
+        hue="population size",
+        palette="deep",
+        style="population size",
+        errorbar="se",
+        teeplot_outdir="batch=${BATCH_UUID}+what=teeplots",
+    )
+
+EOF
+
+
+###############################################################################
+echo "PLOT TIME DATA"
+echo "########################################################################"
+python3 << EOF
+import glob
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+from teeplot import teeplot as tp
+
+
+def plot_timeprof(*args, **kwargs) -> None:
+    plt.gcf().set_size_inches(5, 3)
+    ax = sns.lineplot(*args, errorbar="se", **kwargs)
+    ax = sns.scatterplot(*args, **kwargs, color="salmon", marker="+")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.text(
+        0.95, 0.95,
+        f"n = {kwargs['data']['replicate'].nunique()}",
+        horizontalalignment="right", verticalalignment="top",
+        transform=ax.transAxes,
+    )
+
+
+data = pd.read_csv(
+    "batch=${BATCH_UUID}+what=timeprof+ext=.csv",
+    index_col=False,
+)
+tp.tee(
+    plot_timeprof,
+    data=data,
+    x="population size",
+    y="generations per second",
+    teeplot_outdir="batch=${BATCH_UUID}+what=teeplots",
+)
+EOF
+
+
+###############################################################################
+echo "MAKE TARGZ ARCHIVE"
+echo "########################################################################"
+tar -czvf "batch=${BATCH_UUID}+ext=.tar.gz" "batch=${BATCH_UUID}"*
+
+} | tee batch=${BATCH_UUID}+what=stdout+ext=.txt
---
+++ profile/profile_job.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import itertools as it
+import os
+import sys
+import time
+
+import numpy as np
+import pandas as pd
+
+np.random.seed(1)
+
+from phylotrackpy import systematics
+
+
+# CONFIGURE
+##############################################################################
+run_seconds, mutation_rate = 60, 0.2
+print(f"{run_seconds=}, {mutation_rate=}")
+
+batch_uuid, job_uuid = sys.argv[1:3]
+print(f"{batch_uuid=}, {job_uuid=}")
+
+replicate, pop_size = map(int, sys.argv[3:])
+print(f"{replicate=}, {pop_size=}")
+
+
+# SETUP
+##############################################################################
+population = np.random.uniform(0, 1, pop_size)
+
+sys = systematics.Systematics(str)
+taxa = np.array([sys.add_org(x) for x in population])
+assert sys.get_num_active() == pop_size
+
+start_time = time.time()
+
+# EVOLVE
+##############################################################################
+for generation in it.count():
+    # do selection
+    selections = np.random.randint(0, pop_size, pop_size)
+    next_population = population[selections]
+    # do mutation
+    mutation_mask = np.random.uniform(0, 1, pop_size) < mutation_rate
+    next_population[mutation_mask] = np.random.uniform(
+        0, 1, np.sum(mutation_mask)
+    )
+
+    # elapse generation
+    next_taxa = np.array(
+        [
+            sys.add_org(org, taxa[selection])
+            for org, selection in zip(population, selections)
+        ],
+    )
+    assert len(next_taxa) == pop_size
+    for taxon in taxa:
+        sys.remove_org(taxon)
+
+    taxa = next_taxa
+    population = next_population
+
+    if time.time() - start_time >= run_seconds:
+        break
+
+# SAVE
+##############################################################################
+batch_dir = f"batch={batch_uuid}+what=rawdata"
+os.makedirs(batch_dir, exist_ok=True)
+
+sys.snapshot(f"{batch_dir}/a=systematics+job={job_uuid}+ext=.csv")
+pd.DataFrame(
+    {
+        "replicate": [replicate],
+        "generations elapsed": [generation],
+        "seconds elapsed": [run_seconds],
+        "generations per second": [generation / run_seconds],
+        "population size": [pop_size],
+        "job": [job_uuid],
+        "batch": [batch_uuid],
+    },
+).to_csv(f"{batch_dir}/a=timeprof+job={job_uuid}+ext=.csv", index=False)
