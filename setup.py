f344a4ea3fa4d383aed6b8b719623a173e11c532
Emily Dolson <emilyldolson@gmail.com>
2023-11-25 23:40:33 -0500

    Try removing numpy

--- setup.py
+++ setup.py
@@ -39,8 +39,5 @@ setup(
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires=">=3.7",
-    install_requires=[
-        "numpy",
-    ],
     packages=['phylotrackpy'],
 )
